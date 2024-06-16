17b5cb0fcd467a4aa620a5475006b281486f6769
orichan0222 <158282952+orichan0222@users.noreply.github.com>
2024-06-16 22:12:58 +0900

    Delete portfolio2024 directory

--- portfolio2024/js/index.js
+++
@@ -1,7 +0,0 @@
-// ハンバーガー、ドロワー
-$("#js-button-drawer").on("click",function(){   //#js-button-drawerをクリックした時
-    $(this).toggleClass("is-checked");　　　　  //is-checked(×)を表示させます
-    $("#js-drawer").slideToggle();             //#js-drawerをスライドして表示します
-    $("body").toggleClass("is-fixed");　　　　 //bodyにis-fixed(cssでoverflow:hidden)スクロール
-　　　　　　　　　　　　　　　　　　　　　　　　　　　//できないようにする。
-});
